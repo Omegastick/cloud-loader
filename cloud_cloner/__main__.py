81ca6c3f38cf913a784a34950d51e9a2c08737a9
Omegastick <isaac@poulton.dev>
2022-11-24 17:43:10 +0800

    Add basic functionality

---
+++ cloud_cloner/__main__.py
@@ -0,0 +1,6 @@
+from .bootstrap import app
+from .clone import clone
+from .clone_directory import clone_directory
+
+if __name__ == "__main__":
+    app()
--- cloud_loader/__main__.py
+++
@@ -1,5 +0,0 @@
-from .bootstrap import app
-from .load import load
-
-if __name__ == "__main__":
-    app()
